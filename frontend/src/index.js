a43383ab1c740245171c41f091f76ffdafdc1831
dollekader <dollekader19@gmail.com>
2024-11-23 16:41:16 +0100

    Test ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
+// Tous les imports en haut
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Chart, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, PointElement, LineElement } from 'chart.js';
+ 
+// Enregistrement des éléments, échelles et plugins nécessaires
+Chart.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, PointElement, LineElement);
+ 
+// Rendu de l'application
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ 
 // Supprimez ou commentez les lignes ci-dessous si vous ne les utilisez pas
 // import reportWebVitals from './reportWebVitals';
 // reportWebVitals();
